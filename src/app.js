df632257d96d67aa0c5113c62548fa0194c83cf2
harshit <iamharshitsharma518@gmail.com>
2026-01-11 22:51:34 +0530

    start designing middleware

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const Cookieparser = require('cookie-parser')
 const authRouters = require('./routes/auth.routes')
-
 const app = express()
 app.use(express.json())
 app.use(Cookieparser())
